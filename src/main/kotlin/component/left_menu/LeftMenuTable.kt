48074ad28f4779a033c1dc4fa5beabac3f35e893
AnatolSalau <salovanatoly5@mail.ru>
2024-03-13 16:24:48 +0300

    ChartMainField

--- src/main/kotlin/component/left_menu/LeftMenuTable.kt
+++ src/main/kotlin/component/left_menu/LeftMenuTable.kt
@@ -1,8 +1,18 @@
 package component.left_menu
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
 
 
 @Composable
@@ -11,6 +21,15 @@ fun LeftMenuTable(
     measurementData: Map<Int, Pair<Double, Double>>
 /*    dataMeasure: HashMap<Int, Pair<Double, Double>>*/
 ) {
+    val message = remember{mutableStateOf("")}
+
     Text(text = "Left menu table")
     Text(text = "Some value : ${measurementData}")
+    Text("Text: ${message.value}")
+    OutlinedTextField(
+        modifier = Modifier.width(30.dp),
+        value = message.value,
+        textStyle = TextStyle(),
+        onValueChange = {newText -> message.value = newText}
+    )
 }
