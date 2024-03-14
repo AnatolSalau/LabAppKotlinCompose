95ece5b3bdc23cc30caf6e5fb9d6747910163879
AnatolSalau <salovanatoly5@mail.ru>
2024-03-14 15:24:08 +0300

    LeftMenuTable

--- src/main/kotlin/component/left_menu/LeftMenuTable.kt
+++ src/main/kotlin/component/left_menu/LeftMenuTable.kt
@@ -1,35 +1,45 @@
 package component.left_menu
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
-import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.*
 
 
+@OptIn(ExperimentalUnitApi::class)
 @Composable
 @Preview
 fun LeftMenuTable(
     measurementData: Map<Int, Pair<Double, Double>>
 /*    dataMeasure: HashMap<Int, Pair<Double, Double>>*/
 ) {
-    val message = remember{mutableStateOf("")}
+    val message = remember { mutableStateOf("") }
 
     Text(text = "Left menu table")
     Text(text = "Some value : ${measurementData}")
     Text("Text: ${message.value}")
-    OutlinedTextField(
-        modifier = Modifier.width(30.dp),
+
+    BasicTextField(
         value = message.value,
-        textStyle = TextStyle(),
-        onValueChange = {newText -> message.value = newText}
+        singleLine = true,
+        modifier = Modifier
+            .width(100.dp)
+            .height(100.dp)
+            .wrapContentSize()
+            .border(border = BorderStroke(1.dp, Color.White))
+            .padding(20.dp),
+        textStyle = TextStyle(textAlign = TextAlign.Center),
+        onValueChange = { newText -> message.value = newText }
     )
 }
